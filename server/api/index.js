cea95e880708ab17b67558a186b255b55f876ad9
Taylor Fuqua <tfuqua@gwu.edu>
2018-08-09 21:59:56 -0400

    test running powershell

--- server/api/index.js
+++ server/api/index.js
@@ -15,5 +15,32 @@ export default ({ config }) => {
     });
   });
 
+  api.get('/test', (req, res) => {
+    //const file = fs.readdir(path.join(__dirname, '../scripts/test.ps1'));
+
+    fs.readFile(path.join(__dirname, '../scripts/test.ps1'), (err, file) => {
+      if (err) {
+        console.log(err);
+      }
+
+      console.log(file);
+      let spawn = require('child_process').spawn,
+        child;
+
+      child = spawn('powershell.exe', [file]);
+
+      child.stdout.on('data', function(data) {
+        console.log('Powershell Data: ' + data);
+      });
+      child.stderr.on('data', function(data) {
+        console.log('Powershell Errors: ' + data);
+      });
+      child.on('exit', function() {
+        console.log('Powershell Script finished');
+      });
+      child.stdin.end(); //end input
+    });
+  });
+
   return api;
 };
